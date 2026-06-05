fe20bbb0f578a14ca9df24d25a6c0fac1ab9f571
nakanishi <nnjrg248@gmail.com>
2026-06-05 23:21:14 +0900

    Switch to port 465 SSL

--- MenuSuggestion/settings.py
+++ MenuSuggestion/settings.py
@@ -166,7 +166,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 # 送信元のGmailアドレス
