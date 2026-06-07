b6e3ac2b169334f1b97c3304408e2d9c4313569d
nakanishi <nnjrg248@gmail.com>
2026-06-07 23:48:13 +0900

    いいね機能とかつけたがまだ機能しない

--- Suggestion/models.py
+++ Suggestion/models.py
@@ -9,13 +9,16 @@ class Memo(models.Model):
     content = models.TextField()               # メモ本文
     created_at = models.DateTimeField(auto_now_add=True)  # 作成日時（自動設定）
     updated_at = models.DateTimeField(auto_now=True)      # 更新日時（自動設定）
-
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    views_count = models.IntegerField(default=0) # 閲覧数
     class Meta:#自動生成のルール（アプリ名が blog なら blog_memo等）を無視して、データベース上でのテーブル名を強制的に Memo という名前に固定
         db_table='Memo'
         
+    def total_likes(self):
+        return self.likes.count()   
     def __str__(self):#管理画面などで表示される見出しの文字
         return self.title
     
-class Post(models.Model):  # ここが Post ではなく Article などの場合
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+#class Post(models.Model):  # ここが Post ではなく Article などの場合
+#    title = models.CharField(max_length=200)
+#    content = models.TextField()
