417b2d97c1bea4db6f003fb5fcf9b9c121e6f387
nakanishi <nnjrg248@gmail.com>
2026-06-15 21:42:23 +0900

    BLOGapp に変更

--- MenuSuggestion/settings.py
+++ MenuSuggestion/settings.py
@@ -121,7 +121,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static & Media files (Local)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
